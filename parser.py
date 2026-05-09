8fd5e289d4b1f9fc756d9b85c092efd9e5f59b14
Дмитрий Колодин <dmitrijkolodin@Dmitrijs-MacBook-Air.local>
2026-05-09 16:13:04 +0700

    expand items

--- parser.py
+++ parser.py
@@ -5,12 +5,40 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-ITEMS = [
+ITEMS = ITEMS = [
+
     "AK-47 | Redline (Field-Tested)",
+    "AK-47 | Vulcan (Field-Tested)",
+    "AK-47 | Fire Serpent (Field-Tested)",
+
     "AWP | Asiimov (Battle-Scarred)",
+    "AWP | Dragon Lore (Factory New)",
+    "AWP | Hyper Beast (Field-Tested)",
+
     "M4A4 | Howl (Factory New)",
+    "M4A1-S | Printstream (Field-Tested)",
+
     "Desert Eagle | Blaze (Factory New)",
-    "USP-S | Kill Confirmed (Field-Tested)"
+    "USP-S | Kill Confirmed (Field-Tested)",
+
+    "★ Karambit | Doppler (Factory New)",
+    "★ Butterfly Knife | Fade (Factory New)",
+
+    "Operation Breakout Weapon Case",
+    "CS20 Case",
+    "Gamma Case",
+    "Gamma 2 Case",
+    "Prisma Case",
+    "Prisma 2 Case",
+    "Danger Zone Case",
+    "Recoil Case",
+    "Snakebite Case",
+
+    "Sticker Capsule 2",
+    "Sticker Capsule",
+
+    "DreamHack 2014 Legends (Holo-Foil)",
+    "EMS Katowice 2014 Legends"
 ]
 
 def get_item_data(item_name):
